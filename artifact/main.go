64286fc8b2068d827589d2711068e13c43ea6b43
Marvin Fuchs <marvin.fuchs@sap.com>
2026-04-21 13:19:31 +0200

    Add dockerfile

--- artifact/main.go
+++ artifact/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -12,26 +13,43 @@ import (
 	_ "github.com/ydnar/wasi-http-go/wasihttp" // enable wasi-http
 )
 
+func perform_measurement() error {
+	measurementID := os.Getenv("MEASUREMENT_ID")
+	measurementServer := os.Getenv("MEASUREMENT_SERVER")
+	if measurementID == "" || measurementServer == "" {
+		return errors.New("MEASUREMENT_ID or MEASUREMENT_SERVER is not set")
+	}
+
+	receiverServer := fmt.Sprintf("%s/started/?request=%s", strings.TrimRight(measurementServer, "/"), measurementID)
+
+	resp, err := http.Get(receiverServer)
+	if err != nil {
+		return fmt.Errorf("Error fetching %s: %v", receiverServer, err)
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Printf("Fetched %s with status code: %d\n", receiverServer, resp.StatusCode)
+
+	return nil
+}
+
 func init() {
 	testsuite.Exports.Startuptime = func() (result [2]string) {
-		measurementID := os.Getenv("MEASUREMENT_ID")
-		measurementServer := os.Getenv("MEASUREMENT_SERVER")
-		if measurementID == "" || measurementServer == "" {
-			return [2]string{"", "MEASUREMENT_ID or MEASUREMENT_SERVER is not set"}
-		}
-
-		receiverServer := fmt.Sprintf("%s/started/?request=%s", strings.TrimRight(measurementServer, "/"), measurementID)
-
-		resp, err := http.Get(receiverServer)
+		err := perform_measurement()
 		if err != nil {
-			return [2]string{"", fmt.Sprintf("Error fetching %s: %v", receiverServer, err)}
+			return [2]string{"", err.Error()}
 		}
-		defer resp.Body.Close()
 
-		fmt.Printf("Fetched %s with status code: %d\n", receiverServer, resp.StatusCode)
 		return [2]string{"", ""}
 	}
 }
 
 // main is required for the `wasi` target, even if it isn't used.
-func main() {}
+func main() {
+	err := perform_measurement()
+	if err != nil {
+
+		fmt.Print(err)
+	}
+}
